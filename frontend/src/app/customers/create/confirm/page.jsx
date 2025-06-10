7680472e4b5de1811dccdfd531d58f4d848999d0
k-minamizawa <minamizawa.51031094@gmail.com>
2025-06-10 23:49:28 +0900

    Suspenseのインポート忘れを修正

--- frontend/src/app/customers/create/confirm/page.jsx
+++ frontend/src/app/customers/create/confirm/page.jsx
@@ -3,6 +3,7 @@ import OneCustomerInfoCard from "@/app/components/one_customer_info_card.jsx";
 import fetchCustomer from "./fetchCustomer";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { Suspense} from "react"
 
 export default function ConfirmPageWrapper() {
   return (
